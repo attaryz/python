5ac36dba69d15ed17ac0bc7cf27fe46412f22213
Abdullah Ali <37246509+attaryz@users.noreply.github.com>
2020-07-29 01:21:02 +0200

    first commit

---
+++ lists.py
@@ -0,0 +1,5 @@
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "Septemper", "October", "November", "December"]
+
+first_half = months[:6]
+print(first_half)
+
---
+++ listsV2.py
@@ -0,0 +1,2 @@
+nautical_directions = "\n".join(["fore", "aft", "starboard", "port"])
+print(nautical_directions)
